67cf176d3888996359b372c1ab978758fd2ab04a
TruongHoangNhatHuy <huytruonghoangnhat2002@gmail.com>
2024-10-22 09:46:24 +0700

    django tutorial 5

--- django-tutorial/polls/views.py
+++ django-tutorial/polls/views.py
@@ -1,8 +1,11 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from polls.models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 # helper function for loose coupling
 from django.shortcuts import render, get_object_or_404, get_list_or_404
@@ -13,12 +16,15 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
