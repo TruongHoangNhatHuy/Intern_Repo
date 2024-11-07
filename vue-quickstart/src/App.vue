4877da9faeb4902d41a6b982b97d619a5cd86ed3
TruongHoangNhatHuy <huytruonghoangnhat2002@gmail.com>
2024-11-07 12:27:15 +0700

    vue quickstart 1

--- vue-quickstart/src/App.vue
+++ vue-quickstart/src/App.vue
@@ -1,25 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { computed, ref } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  // import TheWelcome from './components/TheWelcome.vue'
+  import TemplateSyntax from './components/TemplateSyntax.vue'
+  import ReactivityFundamentals from './components/ReactivityFundamentals.vue'
+  import ComputedProperties from './components/ComputedProperties.vue'
+  import ClassStyleBindings from './components/ClassStyleBindings.vue'
+
+  const totalPage = 4
+  const page = ref(1)
+  const pageNum = computed(() => {
+    if (page.value<1) page.value = 1
+    if (page.value>totalPage) page.value = totalPage
+    return page.value
+  })
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="100" height="100" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Vue Quickstart" />
+      <div>
+        <button @click="page--">Prev</button>
+        {{ page }}
+        <button @click="page++">Next</button>
+      </div>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <TemplateSyntax v-show="pageNum==1"/>
+    <ReactivityFundamentals v-show="pageNum==2"/>
+    <ComputedProperties v-show="pageNum==3"/>
+    <ClassStyleBindings v-show="pageNum==4"/>
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+
+main {
+  margin-top: 10px;
 }
 
 .logo {
---
+++ vue-quickstart/src/components/ClassStyleBindings.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const isActive = ref(true)
+  const hasError = ref(false)
+
+  const classObject = computed(() => ({
+    active: isActive.value && !error.value,
+    'text-danger': error.value && error.value.type === 'fatal'
+  }))
+
+  const activeClass = ref('active')
+  const errorClass = ref('text-danger')
+
+  const activeColor = ref('red')
+  const fontSize = ref(30)
+
+  const styleObject = reactive({
+    color: 'red',
+    fontSize: '30px'
+  })
+</script>
+
+<template>
+  <main>
+    <h1>4. Class & Style Binding</h1>
+    <div 
+      class="static"
+      :class="{ active: isActive, 'text-danger': hasError }"
+      :style="{ color: activeColor, fontSize: fontSize+'px' }"
+    >Binding to Objects: {{ `<div class="static active"></div>` }}</div>
+    <div 
+      :class="classObject" 
+      :style="styleObject"
+    > Binding to Computed property: {{ `<div class="active"></div>` }}</div>
+    <div 
+      :class="[activeClass, errorClass]"
+      :style="[baseStyles, overridingStyles]"
+    >Binding to Arrays: {{ `<div class="active text-danger"></div>` }}</div>
+  </main>
+</template>
